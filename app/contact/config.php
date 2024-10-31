3d736408759da4e183382f7f8559e29386da52a4
MardinliG <guillaume.mardinli@ynov.com>
2024-10-31 17:24:37 +0100

    add

--- app/contact/config.php
+++ app/contact/config.php
@@ -1,4 +1,4 @@
 <?php
-define('SMTP_USERNAME','guillaume.mardinli@ynov.com');
-define('SMTP_PASSWORD','Antananarivo*9931');
+define('SMTP_USERNAME','email@email.com'); // Remplacez cette valeur par une adresse mail gmail ou outlook
+define('SMTP_PASSWORD','password'); // Remplacez cette valeur par le mot de passe de l'adresse mail
 ?>
